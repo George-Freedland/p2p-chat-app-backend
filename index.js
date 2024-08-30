12b2cd2254be8074fc2298e7b0bd4041921ec176
George-Freedland <george.freedland@gmail.com>
2024-08-30 05:20:22 -0700

    allow cors

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        // origin: "http://localhost:3000",
+        origin: ["http://192.168.86.28:3000", "http://localhost:3000"],
         methods: ["GET", "POST"]
     }
 });
 
 // app.use(express.static('public'));
+let onlineUsers = 0;
 
 io.on('connection', (socket) => {
     console.log('A user connected');
+    onlineUsers += 1;
+    io.emit('online users', onlineUsers);
     
     socket.on('chat message', ({message, username, color}) => {
         console.log('Message received:', username, ':', message, ' - color: ', color);
@@ -24,6 +28,10 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', () => {
+        if (onlineUsers > 0) {
+            onlineUsers -= 1;
+            io.emit('online users', onlineUsers);
+        }
         console.log('User disconnected');
     })
 })
